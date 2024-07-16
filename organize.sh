4fac91bf2912a8ebdbea731397396741c3a300df
Abdul Rehman <abdulrehmanee010@gmail.com>
2024-07-15 20:11:26 -0400

    .

--- organize.sh
+++ organize.sh
@@ -5,7 +5,7 @@ cd ./songs
 for song in ./*.mp3; do 
   artist=${song/-*/}
   mkdir -p ../script_organized/$artist
-  cp ./$song ../script_organized/$artist
+  ln ./$song ../script_organized/$artist/$song
 done 
 
 
